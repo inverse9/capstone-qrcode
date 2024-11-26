d210452767009e601350798086b5eda4dee8f9b7
inverse9 <permanawahyu97@gmail.com>
2024-11-26 16:19:46 +0800

    menu dashboard

---
+++ src/components/charts/ChartPie.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { PieChart, Pie, ResponsiveContainer, Sector } from "recharts";
+
+const data01 = [
+  { name: "Patung Catur Muka", value: 123 },
+  { name: "Garuda Wisnu Kencana", value: 87 },
+  { name: "Lukisan Pasar Tradisional", value: 12 },
+  { name: "Monumen Bajra Sandhi", value: 378 },
+];
+
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+
+  return (
+    <g>
+      <text
+        className="text-sm"
+        x={cx}
+        y={cy}
+        dy={8}
+        textAnchor="middle"
+        fill={"black"}
+      >
+        {payload.name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="black"
+      >{`${value}`}</text>
+      {/* <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999"
+      >
+        {`(Rate ${(percent * 100).toFixed(2)}%)`}
+      </text> */}
+    </g>
+  );
+};
+
+export const ChartPie = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const onPieEnter = (_, index) => setActiveIndex(index);
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart width={600} height={600}>
+        <Pie
+          activeIndex={activeIndex}
+          activeShape={renderActiveShape}
+          data={data01}
+          cx="50%"
+          cy="50%"
+          innerRadius={80}
+          outerRadius={100}
+          fill="#F88033"
+          dataKey="value"
+          onMouseEnter={onPieEnter}
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import { ChartPie } from "../../components/charts/ChartPie";
+import ChartBar from "../../components/charts/ChartBar";
+import { ChartLine } from "../../components/charts/ChartLine";
 
 const Admin = () => {
-  return <div>admin</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-4 h-[500px]">
+        <div className="flex flex-col w-2/5 h-full">
+          Jumlah Object yang di scan
+          <div className=" w-full h-full border p-4 bg-white rounded-lg">
+            <div className="flex flex-col size-full">
+              <ChartPie />
+              <div className="self-start flex-1">
+                <div className="font-semibold mb-4">Keterangan</div>
+                <div className="grid grid-cols-4 place-start-end">
+                  <div className="col-span-2">Patung Catur Muka :</div>
+                  <div className="pl-3">123</div>
+                </div>
+                <div className="grid grid-cols-4 place-start-end">
+                  <div className="col-span-2">Garuda Wisnu Kencana :</div>
+                  <div className="pl-3">87</div>
+                </div>
+                <div className="grid grid-cols-4 place-start-end">
+                  <div className="col-span-2">Lukisan Pasar Tradisional :</div>
+                  <div className="pl-3">12</div>
+                </div>
+                <div className="grid grid-cols-4 place-start-end">
+                  <div className="col-span-2">Monumen Bajra Sandhi :</div>
+                  <div className="pl-3">378</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" p-4 flex-1">
+          <div className="flex flex-col w-full h-full">
+            Jumlah Object yang di scan
+            <div className=" w-full h-full border p-4 bg-white rounded-lg">
+              <div className="flex flex-col size-full">
+                <ChartLine />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex">
+        <div className=" w-full">
+          Jumlah Object yang di scan
+          <div className=" w-full h-72 border p-4 bg-white rounded-lg">
+            <div className="flex flex-col size-full">
+              <ChartBar />
+              {/* <div className="self-start flex-1">
+                <div className="font-semibold mb-4">Keterangan</div>
+                <div className="grid grid-cols-4 place-start-end">
+                  <div className="col-span-2">Patung Catur Muka :</div>
+                  <div className="pl-3">123</div>
+                </div>
+                <div className="grid grid-cols-4 place-start-end">
+                  <div className="col-span-2">Garuda Wisnu Kencana :</div>
+                  <div className="pl-3">87</div>
+                </div>
+                <div className="grid grid-cols-4 place-start-end">
+                  <div className="col-span-2">Lukisan Pasar Tradisional :</div>
+                  <div className="pl-3">12</div>
+                </div>
+                <div className="grid grid-cols-4 place-start-end">
+                  <div className="col-span-2">Monumen Bajra Sandhi :</div>
+                  <div className="pl-3">378</div>
+                </div>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
