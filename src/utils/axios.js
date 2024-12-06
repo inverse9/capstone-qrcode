f4022f3843a642e569e99a1cf515f21acb7bb410
inverse9 <permanawahyu97@gmail.com>
2024-12-06 17:09:14 +0800

    handle api call from server

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/";
+// const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = "http://realdev1.psti.undiknas.ac.id:3000/api/";
 
 const instance = axios.create({ baseURL: BASE_URL });
 
